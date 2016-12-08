eed55d97938e0572283f9ed84d3e2e138a50fd1e
Matej Gomboc <matej@irnas.eu>
2016-12-08 14:57:56 +0100

    Added test code

--- move_driver_firmware/move_driver_firmware.ino
+++ move_driver_firmware/move_driver_firmware.ino
@@ -11,11 +11,11 @@ Switch limit_switch2(limit_switch2_pin, LOW, false, switch_debounce_delay);
 
 const int unused_gpio_pin = 2;
 
-const int senzor1_pwr_pin = A2;
-const int senzor2_pwr_pin = A3;
+const int sensor1_pwr_pin = A2;
+const int sensor2_pwr_pin = A3;
 
-TLV493D senzor1(senzor1_pwr_pin);
-TLV493D senzor2(senzor2_pwr_pin);
+TLV493D sensor1(sensor1_pwr_pin);
+TLV493D sensor2(sensor2_pwr_pin);
 
 const int stepper1_a_pin = 5;
 const int stepper1_b_pin = 6;
@@ -27,31 +27,74 @@ const int stepper2_b_pin = 9;
 const int stepper2_c_pin = A0;
 const int stepper2_d_pin = A1;
 
-AccelStepper stepper1(AccelStepper::FULL4WIRE, stepper1_a_pin, stepper1_c_pin, stepper1_b_pin, stepper1_d_pin, true);
-AccelStepper stepper2(AccelStepper::FULL4WIRE, stepper2_a_pin, stepper2_c_pin, stepper2_b_pin, stepper2_d_pin, true);
+AccelStepper stepper1(AccelStepper::FULL4WIRE, stepper1_a_pin, stepper1_c_pin, stepper1_b_pin, stepper1_d_pin, false);
+AccelStepper stepper2(AccelStepper::FULL4WIRE, stepper2_a_pin, stepper2_c_pin, stepper2_b_pin, stepper2_d_pin, false);
 
 void setup()
 {
-  Serial.begin(115200); // begin serial connection for debug
+  Serial.begin(115200);
 
   pinMode(unused_gpio_pin, INPUT);
 
-  senzor1.init(LOW);
-  senzor2.init(HIGH);
+  sensor1.init(LOW);
+  sensor2.init(HIGH);
 
   stepper1.setMaxSpeed(100);
   stepper1.setSpeed(10);
   stepper1.setAcceleration(100);
-  stepper1.moveTo(0);
+  stepper1.moveTo(10000);
   stepper1.enableOutputs();
 
   stepper2.setMaxSpeed(100);
   stepper2.setSpeed(10);
   stepper2.setAcceleration(100);
-  stepper2.moveTo(0);
+  stepper2.moveTo(10000);
   stepper2.enableOutputs();
 }
 
 void loop()
 {
+  sensor1.update();
+  Serial.println("sensor1:");
+
+  Serial.print(sensor1.m_dBx);
+  Serial.print(";");//\t");
+  Serial.print(sensor1.m_dBy);
+  Serial.print(";");//\t");
+  Serial.print(sensor1.m_dBz);
+  Serial.print(";");//\t");
+  Serial.println(sensor1.m_dTemp);
+
+  Serial.print(sensor1.m_dPhi_xy);
+  Serial.print(";");//\t");
+  Serial.print(sensor1.m_dPhi_yz);
+  Serial.print(";");//\t");
+  Serial.print(sensor1.m_dPhi_xz);
+  Serial.print(";");//\t");
+  Serial.println(sensor1.m_dMag_2);
+
+  sensor2.update();
+  Serial.println("sensor2:");
+
+  Serial.print(sensor2.m_dBx);
+  Serial.print(";");//\t");
+  Serial.print(sensor2.m_dBy);
+  Serial.print(";");//\t");
+  Serial.print(sensor2.m_dBz);
+  Serial.print(";");//\t");
+  Serial.println(sensor2.m_dTemp);
+
+  Serial.print(sensor2.m_dPhi_xy);
+  Serial.print(";");//\t");
+  Serial.print(sensor2.m_dPhi_yz);
+  Serial.print(";");//\t");
+  Serial.print(sensor2.m_dPhi_xz);
+  Serial.print(";");//\t");
+  Serial.println(sensor2.m_dMag_2);
+  
+  if(limit_switch1.get_button_state()) Serial.println("Limit switch 1 pressed.");
+  if(limit_switch2.get_button_state()) Serial.println("Limit switch 2 pressed.");
+  
+  stepper1.run();
+  stepper2.run();
 }
