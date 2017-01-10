2b986cf1bf43f6c4a156aafc158566067b6d5d05
Matej Gomboc <matej@irnas.eu>
2017-01-10 20:36:45 +0100

    implemented Homing class

--- move_driver_firmware/homing.cpp
+++ move_driver_firmware/homing.cpp
@@ -2,74 +2,121 @@
 #include "AccelStepper.h" // http://www.airspayce.com/mikem/arduino/AccelStepper/
 #include "homing.h"
 
-
-extern Switch limit_switch1;
-extern Switch limit_switch2;
-extern AccelStepper stepper1;
-extern AccelStepper stepper2;
-
-void homing()
+Homing::Homing(Switch& limit_switch1, Switch& limit_switch2, AccelStepper& stepper1, AccelStepper& stepper2) :
+	m_limit_switch1(limit_switch1),
+	m_limit_switch2(limit_switch2),
+	m_stepper1(stepper1),
+	m_stepper2(stepper2)
 {
-  //Serial.println("Homing started.");
+	m_state = HomingState::STANDBY;
+	m_status = HomingStatus::OK;
+}
 
-  stepper1.setCurrentPosition(0);
-  stepper1.setMaxSpeed(1000);
-  stepper1.setSpeed(100);
-  stepper1.setAcceleration(50);
-  stepper1.move(-1000000);
-  stepper1.enableOutputs();
 
-  stepper2.setCurrentPosition(0);
-  stepper2.setMaxSpeed(1000);
-  stepper2.setSpeed(100);
-  stepper2.setAcceleration(50);
-  stepper2.move(-1000000);
-  stepper2.enableOutputs();
+HomingState Homing::currentState()
+{
+	return m_state;
+}
 
-  while (true)
-  {
-    if ((limit_switch1.get_button_state() == true) && (stepper1.distanceToGo() < 0))
-    {
-      //Serial.println("Limit switch 1 pressed.");
-      delay(1000);
-      stepper1.setCurrentPosition(0);
-      stepper1.setMaxSpeed(1000);
-      stepper1.setSpeed(100);
-      stepper1.setAcceleration(50);
-      stepper1.move(10000);
-      continue;
-    }
 
-    if ((limit_switch2.get_button_state() == true) && (stepper2.distanceToGo() < 0))
-    {
-      //Serial.println("Limit switch 2 pressed.");
-      delay(1000);
-      stepper2.setCurrentPosition(0);
-      stepper2.setMaxSpeed(1000);
-      stepper2.setSpeed(100);
-      stepper2.setAcceleration(50);
-      stepper2.move(10000);
-      continue;
-    }
+HomingStatus Homing::currentStatus()
+{
+	return m_status;
+}
 
-    //if (stepper1.distanceToGo() != 0) Serial.println("Stepper 1 moving.");
-    stepper1.run();
 
-    //if (stepper2.distanceToGo() != 0) Serial.println("Stepper 2 moving.");
-    stepper2.run();
+void Homing::reset()
+{
+	m_stepper1.setCurrentPosition(0);
+	m_stepper1.setMaxSpeed(1000);
+	m_stepper1.setSpeed(100);
+	m_stepper1.setAcceleration(50);
+	m_stepper1.move(-1000000);
+	m_stepper1.enableOutputs();
 
-    if (stepper1.distanceToGo() == 0)
-    {
-      //Serial.println("Homing 1 finished.");
-      stepper1.disableOutputs();
-    }
+	m_stepper2.setCurrentPosition(0);
+	m_stepper2.setMaxSpeed(1000);
+	m_stepper2.setSpeed(100);
+	m_stepper2.setAcceleration(50);
+	m_stepper2.move(-1000000);
+	m_stepper2.enableOutputs();
+	
+	m_state = HomingState::STANDBY;
+	m_status = HomingStatus::OK;
+}
 
-    if (stepper2.distanceToGo() == 0)
-    {
-      //Serial.println("Homing 2 finished.");
-      stepper2.disableOutputs();
-    }
 
-    if ((stepper1.distanceToGo() == 0) && (stepper2.distanceToGo() == 0)) return;
-  }
+void Homing::start()
+{
+	switch(m_state)
+	{
+		case (HomingState::STANDBY):
+		{
+			m_state = HomingState::INPROGRESS;
+			break;
+		}
+		case (HomingState::INPROGRESS):
+		{
+			break;
+		}
+		case (HomingState::ERROR):
+		{
+			reset();
+			start();
+			break;
+		}
+	}
+}
+
+
+void Homing::process()
+{
+	switch(m_state)
+	{
+		case (HomingState::STANDBY):
+		{
+			break;
+		}
+		case (HomingState::INPROGRESS):
+		{
+			if ((limit_switch1.get_button_state() == true) && (m_stepper1.distanceToGo() < 0))
+			{
+			  m_stepper1.setCurrentPosition(0);
+			  m_stepper1.setMaxSpeed(1000);
+			  m_stepper1.setSpeed(100);
+			  m_stepper1.setAcceleration(50);
+			  m_stepper1.move(10000);
+			  
+			  break;
+			}
+
+			if ((limit_switch2.get_button_state() == true) && (m_stepper2.distanceToGo() < 0))
+			{
+			  m_stepper2.setCurrentPosition(0);
+			  m_stepper2.setMaxSpeed(1000);
+			  m_stepper2.setSpeed(100);
+			  m_stepper2.setAcceleration(50);
+			  m_stepper2.move(10000);
+			  
+			  break;
+			}
+
+			m_stepper1.run();
+			m_stepper2.run();
+
+			if ((m_stepper1.distanceToGo() == 0) && (m_stepper2.distanceToGo() == 0))
+			{
+				m_stepper1.disableOutputs();
+				m_stepper2.disableOutputs();
+				m_state = HomingState::STANDBY;
+				return;
+			}
+			
+			break;
+		}
+		case (HomingState::ERROR):
+		{
+			break;
+		}
+	}
 }
--- move_driver_firmware/homing.h
+++ move_driver_firmware/homing.h
@@ -1,6 +1,39 @@
 #ifndef homing_h
 #define homing_h
 
-extern void homing();
+#include "switch.h" // https://github.com/IRNAS/Arduino-Switch-Debounce
+#include "AccelStepper.h" // http://www.airspayce.com/mikem/arduino/AccelStepper/
+
+enum class HomingState
+{
+	STANDBY,
+	INPROGRESS,
+	ERROR
+};
+
+enum class HomingStatus
+{
+	OK,
+	ERROR
+};
+
+class Homing
+{
+private:
+	Switch& m_limit_switch1;
+	Switch& m_limit_switch2;
+	AccelStepper& m_stepper1;
+	AccelStepper& m_stepper2;
+	
+	HomingState m_state;
+	HomingStatus m_status;
+public:
+	Homing(Switch& limit_switch1, Switch& limit_switch2, AccelStepper& stepper1, AccelStepper& stepper2);
+	HomingState currentState();
+	HomingStatus currentStatus();
+	void process();
+	void reset();
+	void start();
+};
 
 #endif
