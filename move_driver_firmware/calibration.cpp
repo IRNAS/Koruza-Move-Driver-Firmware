b2ab8df8e896fd510d72145ebf97d4a3b65950dc
Matej Gomboc <matej@irnas.eu>
2017-01-11 13:06:09 +0100

    added Calibration state machine

--- move_driver_firmware/calibration.cpp
+++ move_driver_firmware/calibration.cpp
@@ -17,6 +17,33 @@ Calibration::Calibration(Switch& limit_switch, TLV493D& sensor, AccelStepper& st
   m_end_point(0.0),
   m_start_step(0)
 {
+	m_state = CalibrationState::STANDBY;
+	m_status = CalibrationStatus::OK;
+
+    // calibration parameters
+    m_start_step = 0;
+    m_start_point = 0;
+    m_end_point = 1.0;
+	
+	m_N_cal_points = m_steps_per_revolution;
+	m_current_point = 0;
+	
+	m_angle_meas_unwrap = 0.0;
+    m_interval = 0;
+    m_prev_value = 0.0;
+}
+
+
+
+CalibrationState Homing::currentState()
+{
+	return m_state;
+}
+
+
+CalibrationStatus Homing::currentStatus()
+{
+	return m_status;
 }
 
 
@@ -73,98 +100,6 @@ uint8_t Calibration::wrap(long& value, const long min_value, const long max_valu
 }
 
 
-uint8_t Calibration::calibrate(const long N_points)
-{
-  //return status:
-  //  0x00 = OK
-  //  0x01 = sensor error, invalid num. of bytes received
-  //  0x02 = sensor error, sensor busy
-  //  0x03 = invalid num. of points given
-  //  0x04 = limit switch pressed
-
-  //Serial.println("Calibration started.");
-
-  if ((N_points > m_steps_per_revolution) || (N_points < 2)) return 0x03; // error
-
-  double angle_meas_unwrap = 0.0;
-  long interval = 0;
-  double prev_value = 0.0;
-
-  int count_tries = 1000;
-  uint8_t status_sensor = 0x00;
-
-  m_start_step = m_stepper.currentPosition();
-  //Serial.print("Starting step: ");
-  //Serial.println(m_start_step);
-
-  m_stepper.setSpeed(100);
-  m_stepper.setAcceleration(50);
-  m_stepper.enableOutputs();
-
-  for (long i = 0; i < N_points; i++)
-  {
-    m_stepper.moveTo(m_start_step + i * m_steps_per_revolution / (N_points - 1));
-
-    while (true)
-    {
-      if (m_limit_switch.get_button_state() == true) return 0x04;
-      m_stepper.run();
-
-      if (!m_stepper.isRunning())
-      {
-        // try to update sensor value
-        while (count_tries > 0)
-        {
-          status_sensor = m_sensor.update();
-          if (status_sensor != 0x02) break;
-          count_tries --;
-        }
-        if (status_sensor != 0x00) return status_sensor; // error
-
-        if (i == 0) prev_value = m_sensor.m_dPhi_xz;
-
-        //Serial.print(i * m_steps_per_revolution / (N_points - 1));
-        //Serial.print(";");
-
-        angle_meas_unwrap = phase_unwrap(m_sensor.m_dPhi_xz, prev_value, interval);
-        prev_value = angle_meas_unwrap;
-        
-        //Serial.print(angle_meas_unwrap, 10);
-        //Serial.print(";");
-        //Serial.print(m_sensor.m_dPhi_xy, 10);
-        //Serial.print(";");
-        //Serial.print(m_sensor.m_dPhi_yz, 10);
-        //Serial.print(";");
-        //Serial.print(m_sensor.m_dPhi_xz, 10);
-        //Serial.print(";");
-        //Serial.println(m_sensor.m_dMag_2, 10);
-
-        break;
-      }
-    }
-
-    if (i == 0) m_start_point = angle_meas_unwrap;
-  }
-
-  m_end_point = angle_meas_unwrap;
-
-  // move back to starting position
-  m_stepper.move(-m_steps_per_revolution);
-  double sezor_reading = 0.0;
-
-  while (true)
-  {
-    if (m_limit_switch.get_button_state() == true) return 0x04;
-    m_stepper.run();
-    if (!m_stepper.isRunning()) break;
-  }
-  
-  m_stepper.disableOutputs();
-
-  return 0x00; // success
-}
-
-
 uint8_t Calibration::calculate_step(long& motor_step)
 {
   //return status:
@@ -190,4 +125,150 @@ uint8_t Calibration::calculate_step(long& motor_step)
   if (wrap(motor_step, 0, m_steps_per_revolution) != 0x00) return 0x03; // error
 
   return 0x00; // success
+}
+
+
+void Calibration::reset()
+{
+	m_state = CalibrationState::STANDBY;
+	m_status = CalibrationStatus::OK;
+	m_stepper.disableOutputs();
+}
+
+
+bool Calibration::start(const long N_points)
+{
+	if ((N_points > m_steps_per_revolution) || (N_points < 2)) return true; // error
+
+	switch(m_state)
+	{
+		case(CalibrationState::STANDBY):
+		{
+			m_N_cal_points = N_points;
+			m_current_point = 0;
+			
+			m_stepper.setSpeed(100);
+			m_stepper.setAcceleration(50);
+			m_stepper.enableOutputs();
+			
+			m_state = CalibrationState::NEXT_POINT;
+			break;
+		}
+		case(!CalibrationState::STANDBY):
+		{
+			reset();
+			return start(N_points);
+		}
+		default:
+		{
+			break;
+		}
+	}
+	
+	return false; // success
+}
+
+
+void Calibration::process()
+{
+	switch(m_state)
+	{
+		case(CalibrationState::STANDBY):
+		{
+			break;
+		}
+		case(CalibrationState::NEXT_POINT):
+		{
+			// movement relative to current position
+			m_stepper.moveTo(m_start_step + m_current_point * m_steps_per_revolution / (m_N_cal_points - 1));
+			
+			m_state = CalibrationState::RUN_MOTOR;
+			
+			break;
+		}
+		case(CalibrationState::RUN_MOTOR):
+		{
+			if (m_limit_switch.get_button_state() == true)
+			{
+				m_status = CalibrationStatus::LIMIT_SWITCH_PRESSED;
+				m_state = CalibrationState::ERROR;
+				break;
+			}
+			if (!m_stepper.isRunning())
+			{
+				m_state = CalibrationState::READ_SENSOR;
+				break;
+			}
+			
+			m_stepper.run();
+	  
+			break;
+		}
+		case(CalibrationState::READ_SENSOR):
+		{
+			int count_tries = 1000;
+			uint8_t status_sensor = 0x00;
+			
+			// try to update sensor value
+			while (count_tries > 0)
+			{
+				status_sensor = m_sensor.update();
+				if (status_sensor != 0x02) break;
+				count_tries --;
+			}
+			
+			switch(status_sensor)
+			{
+				case(0x00):
+				{
+					if (m_current_point == 0)
+					{
+						m_interval = 0;
+						m_prev_value = m_sensor.m_dPhi_xz;
+						m_start_step = m_stepper.currentPosition();
+					}
+					
+					m_angle_meas_unwrap = phase_unwrap(m_sensor.m_dPhi_xz, m_prev_value, m_interval);
+					m_prev_value = m_angle_meas_unwrap;
+					
+					if (m_current_point == 0) m_start_point = m_angle_meas_unwrap;
+					
+					if (m_current_point < m_N_cal_points) m_current_point ++;
+					else
+					{
+						m_end_point = m_angle_meas_unwrap;
+						m_stepper.disableOutputs();
+						m_status = CalibrationStatus::STANDBY;
+					}
+					break;
+				}
+				case(0x01):
+				{
+					m_status = CalibrationStatus::SENSOR_COMM_ERROR;
+					m_state = CalibrationState::ERROR;
+					break;
+				}
+				case(0x02):
+				{
+					m_status = CalibrationStatus::SENSOR_BUSSY;
+					m_state = CalibrationState::ERROR;
+					break;
+				}
+				default:
+				{
+					break;
+				}
+			}
+		
+			break;
+		}
+		case(CalibrationState::ERROR):
+		{
+			break;
+		}
+		default:
+		{
+			break;
+		}
+	}
 }
--- move_driver_firmware/calibration.h
+++ move_driver_firmware/calibration.h
@@ -5,15 +5,42 @@
 #include "TLV493D.h" // https://github.com/IRNAS/TLV493D-3D-Magnetic-Sensor-Arduino-Library
 #include "AccelStepper.h" // http://www.airspayce.com/mikem/arduino/AccelStepper/
 
+
+enum class CalibrationState
+{
+	STANDBY,
+	NEXT_POINT,
+	RUN_MOTOR,
+	READ_SENSOR,
+	ERROR
+};
+
+enum class CalibrationStatus
+{
+	OK,
+	SENSOR_COMM_ERROR,
+	SENSOR_BUSSY,
+	LIMIT_SWITCH_PRESSED
+};
+
 class Calibration
 {
   private:
     Switch& m_limit_switch;
     TLV493D& m_sensor;
     AccelStepper& m_stepper;
-    
+	
+	CalibrationState m_state;
+	CalibrationStatus m_status;
+	
+	long m_N_cal_points;
+	long m_current_point;
+	double m_angle_meas_unwrap;
+    long m_interval;
+    double m_prev_value;
+	
   public:
-    // calibration parameters
+	// calibration parameters
     long m_start_step;
     static const long m_steps_per_revolution;
     double m_start_point;
@@ -21,8 +48,12 @@ class Calibration
     
   public:
     Calibration(Switch& limit_switch, TLV493D& sensor, AccelStepper& stepper);
-    uint8_t calibrate(const long N_points);
+    bool start(const long N_points);
+	void reset();
     uint8_t calculate_step(long& motor_step);
+	void process();
+	CalibrationState currentState();
+	CalibrationStatus currentStatus();
 
   private:
     double phase_unwrap(const double angle_0_2pi, const double prev_value, long& interval);
