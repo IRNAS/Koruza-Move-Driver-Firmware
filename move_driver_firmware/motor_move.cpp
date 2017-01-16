b9ea30a4ab462737ed49ad5fce0c958fa8b5e7c5
VojislavM <milivojevicvoja@yahoo.com>
2017-01-16 09:55:23 +0100

    add motor_move class

---
+++ move_driver_firmware/motor_move.cpp
@@ -0,0 +1,144 @@
+#include "switch.h" // https://github.com/IRNAS/Arduino-Switch-Debounce
+#include "AccelStepper.h" // http://www.airspayce.com/mikem/arduino/AccelStepper/
+#include "motor_move.h"
+
+
+String convertToString(const MotorMoveState& state)
+{
+  switch (state)
+  {
+    case (MotorMoveState::STANDBY): return (String("STANDBY"));
+    case (MotorMoveState::MOVING): return (String("MOVING"));
+    case (MotorMoveState::ERROR): return (String("ERROR"));
+  }
+}
+
+
+String convertToString(const MotorMoveStatus& status)
+{
+  switch (status)
+  {
+    case (MotorMoveStatus::OK): return (String("OK"));
+    case (MotorMoveStatus::LIMIT_SWITCH_PRESSED): return (String("LIMIT_SWITCH_PRESSED"));
+  }
+}
+
+
+MotorMove::MotorMove(Switch& limit_switch, AccelStepper& stepper) :
+  m_limit_switch(limit_switch),
+  m_stepper(stepper)
+{
+  m_state = MotorMoveState::STANDBY;
+  m_status = MotorMoveStatus::OK;
+}
+
+
+MotorMoveState MotorMove::currentState()
+{
+  return m_state;
+}
+
+
+MotorMoveStatus MotorMove::currentStatus()
+{
+  return m_status;
+}
+
+
+void MotorMove::reset()
+{
+  m_stepper.setMaxSpeed(1000);
+  m_stepper.setSpeed(100);
+  m_stepper.setAcceleration(50);
+  m_stepper.disableOutputs();
+
+  m_state = MotorMoveState::STANDBY;
+  m_status = MotorMoveStatus::OK;
+}
+
+
+void MotorMove::move(long relative)
+{
+  switch (m_state)
+  {
+    case (MotorMoveState::STANDBY):
+      {
+		m_stepper.move(relative);
+		m_stepper.enableOutputs();
+        m_state = MotorMoveState::MOVING;
+        break;
+      }
+    case (MotorMoveState::MOVING):
+      {
+        break;
+      }
+    case (MotorMoveState::ERROR):
+      {
+        reset();
+        move(relative);
+        break;
+      }
+  }
+}
+
+
+void MotorMove::moveTo(long absolute)
+{
+  switch (m_state)
+  {
+    case (MotorMoveState::STANDBY):
+      {
+		m_stepper.moveTo(absolute);
+		m_stepper.enableOutputs();
+        m_state = MotorMoveState::MOVING;
+        break;
+      }
+    case (MotorMoveState::MOVING):
+      {
+        break;
+      }
+    case (MotorMoveState::ERROR):
+      {
+        reset();
+        moveTo(absolute);
+        break;
+      }
+  }
+}
+
+
+void MotorMove::process()
+{
+  switch (m_state)
+  {
+    case (MotorMoveState::STANDBY):
+      {
+        break;
+      }
+    case (MotorMoveState::MOVING):
+      {
+        if ((m_limit_switch.get_button_state() == true) && (m_stepper.distanceToGo() < 0))
+        {
+		  m_stepper.disableOutputs();
+          m_state = MotorMoveState::ERROR;
+		  m_status = MotorMoveStatus::LIMIT_SWITCH_PRESSED;
+          break;
+        }
+
+        m_stepper.run();
+
+        if (m_stepper.distanceToGo() == 0)
+        {
+          m_stepper.disableOutputs();
+          m_state = MotorMoveState::STANDBY;
+          return;
+        }
+
+        break;
+      }
+    case (MotorMoveState::ERROR):
+      {
+        break;
+      }
+  }
+}
---
+++ move_driver_firmware/motor_move.h
@@ -0,0 +1,41 @@
+#ifndef motor_move_h
+#define motor_move_h
+
+#include "switch.h" // https://github.com/IRNAS/Arduino-Switch-Debounce
+#include "AccelStepper.h" // http://www.airspayce.com/mikem/arduino/AccelStepper/
+
+enum class MotorMoveState
+{
+  STANDBY,
+  MOVING,
+  ERROR
+};
+
+enum class MotorMoveStatus
+{
+  OK,
+  LIMIT_SWITCH_PRESSED
+};
+
+String convertToString(const MotorMoveState& state);
+String convertToString(const MotorMoveStatus& status);
+
+class MotorMove
+{
+  private:
+    Switch& m_limit_switch;
+    AccelStepper& m_stepper;
+
+    MotorMoveState m_state;
+    MotorMoveStatus m_status;
+  public:
+    MotorMove(Switch& limit_switch, AccelStepper& stepper);
+    MotorMoveState currentState();
+    MotorMoveStatus currentStatus();
+    void process();
+    void reset();
+    void move(long relative);
+	void moveTo(long absolute);
+};
+
+#endif
