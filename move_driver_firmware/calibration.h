5c88f7a03fb36f237e3b3375de84a4e28e8d7c27
VojislavM <milivojevicvoja@yahoo.com>
2017-05-23 12:03:08 +0200

    travis test

--- move_driver_firmware/calibration.h
+++ move_driver_firmware/calibration.h
@@ -2,9 +2,10 @@
 #define calibration_h
 
 #include "Arduino.h"
+#include "AccelStepper.h" // http://www.airspayce.com/mikem/arduino/AccelStepper/
 #include "switch.h" // https://github.com/IRNAS/Arduino-Switch-Debounce
 #include "TLV493D.h" // https://github.com/IRNAS/TLV493D-3D-Magnetic-Sensor-Arduino-Library
-#include "AccelStepper.h" // http://www.airspayce.com/mikem/arduino/AccelStepper/
+
 
 
 enum class CalibrationState
